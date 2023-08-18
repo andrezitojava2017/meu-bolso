3bd74382c186d1d3cdb9b8f389445c4b2d1d6857
Jederson Andre <andre_sjx@live.com>
2023-08-17 22:06:27 -0300

    feat: implementação da tela de cadastro inicial do usuario; tambem foi implementado as chamadas as funções para salvar dados no sqlite

--- src/screens/cadastro/cadastro.js
+++ src/screens/cadastro/cadastro.js
@@ -1,11 +1,51 @@
 import React, {useState} from 'react';
-import {Image, Text, TextInput, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import estilo from './estilo/estilo';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {Button, HStack, Input, VStack} from 'native-base';
+import {mascaraTelefone, validaCampos} from './funcoes/acoes';
+import {
+  criarTabelaUsuario,
+  salvarNovoUsuario,
+} from '../../database/tableUsuario';
+import {ToastAndroid} from 'react-native';
 
-const Cadastro = () => {
+const Cadastro = ({navigation}) => {
   const [aguardando, setAguardando] = useState(false);
+  const [nome, setNome] = useState('');
+  const [telefone, setTelefone] = useState('');
+
+  const formatarTelefone = value => {
+    let tel = mascaraTelefone(value);
+    setTelefone(tel);
+  };
+
+  const salvarSqlite = async () => {
+    setAguardando(true);
+
+    /** valida campo nome */
+    try {
+      validaCampos(nome, telefone);
+    } catch (error) {
+      ToastAndroid.showWithGravity(error.message, 5000, ToastAndroid.CENTER);
+      setAguardando(false);
+      return;
+    }
+
+    /**Salva dados na base de dados */
+    try {
+      await criarTabelaUsuario();
+      await salvarNovoUsuario([nome, telefone]);
+      setAguardando(false);
+    } catch (error) {
+      ToastAndroid.showWithGravity(
+        'Ocorreu um erro na captura de dados do usuario!',
+        5000,
+        ToastAndroid.BOTTOM,
+      );
+      setAguardando(false);
+    }
+  };
 
   return (
     <View style={estilo.container}>
@@ -27,12 +67,17 @@ const Cadastro = () => {
         <Text style={estilo.viewInfo.text}>Preencha as informações</Text>
         <VStack alignItems={'center'} space={4}>
           <Input
+            onChangeText={value => setNome(value)}
+            value={nome}
             size="lg"
             placeholder="Seu nome"
             maxWidth="80%"
             InputLeftElement={<Icon name="account" size={30} color="#0C7489" />}
           />
           <Input
+            keyboardType={'numeric'}
+            onChangeText={formatarTelefone}
+            value={telefone}
             size="lg"
             placeholder="Telefone"
             maxWidth="80%"
@@ -43,14 +88,11 @@ const Cadastro = () => {
               <Button
                 isLoading={aguardando}
                 isLoadingText="Aguarde"
-                onPress={() => setAguardando(false)}>
+                onPress={salvarSqlite}>
                 PRONTO
               </Button>
             ) : (
-              <Button
-                size={'md'}
-                width={180}
-                onPress={() => setAguardando(true)}>
+              <Button size={'md'} width={180} onPress={salvarSqlite}>
                 PRONTO
               </Button>
             )}
