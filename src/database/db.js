a4043ac9ba7fc62457009f06f7833d3b5c2b8d99
Jederson Andre <andre_sjx@live.com>
2023-08-19 22:56:24 -0300

    refactor: alteracao para tratar funcao assincrona

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,9 @@
 import SQLite from 'react-native-sqlite-storage';
 
-export const conectarDataBase = () => {
-  const db = SQLite.openDatabase({name: 'meubolso.db', location: 'default'});
+export const conectarDataBase = async () => {
+  const db = await SQLite.openDatabase({
+    name: 'meubolso.db',
+    location: 'default',
+  });
   return db;
 };
