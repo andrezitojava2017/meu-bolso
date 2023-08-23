364faefe8c72fe1ced9a21740a2a0912d6819fed
Jederson Andre <andre_sjx@live.com>
2023-08-23 12:02:17 -0300

    refactor: inclusao da coluna perfil na tabela de usuarios

--- src/database/tableUsuario.js
+++ src/database/tableUsuario.js
@@ -5,7 +5,7 @@ export const criarTabelaUsuario = async () => {
 
   db.transaction(tx => {
     tx.executeSql(
-      `CREATE TABLE IF NOT EXISTS usuario (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, contato VARCHAR)`,
+      `CREATE TABLE IF NOT EXISTS usuario (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, contato VARCHAR, perfil VARCHAR)`,
       [],
       result => {
         return true;
@@ -37,7 +37,7 @@ export const buscarUsuario = async () => {
         (tx, result) => {
           var linhas = result.rows;
           for (let i = 0; i < linhas.length; i++) {
-            //console.log(linhas.item(i));
+           // console.log(linhas.item(i));
             res.push(linhas.item(i));
           }
           resolve(res);
@@ -57,7 +57,7 @@ export const buscarUsuario = async () => {
 export const salvarNovoUsuario = async data => {
   const db = await conectarDataBase();
 
-  let sql = `insert into usuario (nome, contato) values (?,?)`;
+  let sql = `insert into usuario (nome, contato, perfil) values (?,?,?)`;
 
   // return new Promise((resolve, reject) => {
   await db.transaction(tx => {
@@ -69,6 +69,32 @@ export const salvarNovoUsuario = async data => {
       },
       error => {
         console.log('Não foi possivel inserir os dados do usuario: ', error);
+        throw error;
+      },
+      () => {
+        db.close();
+      },
+    );
+  });
+
+  // });
+};
+
+export const deleteTable = async data => {
+  const db = await conectarDataBase();
+
+  let sql = `drop table usuario`;
+
+  // return new Promise((resolve, reject) => {
+  await db.transaction(tx => {
+    tx.executeSql(
+      sql,
+      [data],
+      result => {
+        console.log('DELETADO COM SUCESSO ');
+      },
+      error => {
+        console.log('Não foi possivel DELETAR tabela de usuarios: ', error);
         throw error;
       },
       () => {
