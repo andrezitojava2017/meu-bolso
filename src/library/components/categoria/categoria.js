6323597994ee900f8cafcaaee969c00f5be7e5ee
Jederson Andre <andre_sjx@live.com>
2023-08-25 16:47:25 -0300

    feat: implementado o componente Categoria, que ira exibir na screen despesas as categorias disponiveis na base de dados

---
+++ src/library/components/categoria/categoria.js
@@ -0,0 +1,64 @@
+import {Box, Text} from 'native-base';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import colors from 'res/colors';
+
+const Categoria = () => {
+  return (
+    <Box
+      width={80}
+      bg={colors.secundary_background}
+      rounded="md"
+      shadow={3}
+      marginTop={4}>
+      <Text color={colors.primary_background} textAlign={'center'} fontSize={18}>
+        Categoria
+      </Text>
+
+      <Box
+        width={'100%'}
+        flexDirection={'row'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        flexWrap={'wrap'}>
+        <Box padding={2}>
+          <Icon name="calendar" size={24} color={colors.primary_background} />
+          <Text fontSize={10}>Transporte</Text>
+        </Box>
+        <Box padding={2}>
+          <Icon name="calendar" size={24} color={colors.primary_background} />
+          <Text fontSize={10}>Viagem</Text>
+        </Box>
+         <Box padding={2}>
+          <Icon name="calendar" size={24} color={colors.primary_background} />
+          <Text fontSize={10}>Mercado</Text>
+        </Box>
+         <Box padding={2}>
+          <Icon name="calendar" size={24} color={colors.primary_background} />
+          <Text fontSize={10}>Mercado</Text>
+        </Box>
+         <Box padding={2}>
+          <Icon name="calendar" size={24} color={colors.primary_background} />
+          <Text fontSize={10}>Mercado</Text>
+        </Box>
+         <Box padding={2}>
+          <Icon name="calendar" size={24} color={colors.primary_background} />
+          <Text fontSize={10}>Mercado</Text>
+        </Box>
+
+         <Box padding={2}>
+          <Icon name="calendar" size={24} color={colors.primary_background} />
+          <Text fontSize={10}>Mercado</Text>
+        </Box>
+         <Box padding={2}>
+          <Icon name="calendar" size={24} color={colors.primary_background} />
+          <Text fontSize={10}>Mercado</Text>
+        </Box>
+         <Box padding={2}>
+          <Icon name="calendar" size={24} color={colors.primary_background} />
+          <Text fontSize={10}>Mercado</Text>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+export default Categoria;
---
+++ src/library/components/categoria/estilo.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+import colors from "../../../res/colors";
+
+export const estilo = StyleSheet.create({
+container:{
+    flex:1,
+    backgroundColor: colors.primary_background,
+}
+})
